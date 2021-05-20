cfc17e0492445c414890e3afbfc6a4e39a894889
vetszabolcs <70918904+vetszabolcs@users.noreply.github.com>
2021-05-20 11:22:09 +0200

    Update code.R

--- code.R
+++ code.R
@@ -15,7 +15,7 @@ orig$generation <-  factor(orig$generation, levels = c("G.I. Generation", "Silen
                                                        "Generation X", "Millenials", "Generation Z"))
 
 
-#personalised aggregate function
+# personalised aggregate function
 aggreGate <- function(data_f, by="country", by2="year"){
   new_df <- data_f
   for (x in unique(new_df[,by])){
@@ -26,13 +26,13 @@ aggreGate <- function(data_f, by="country", by2="year"){
   return(new_df)}
 
 
-#handling possible font error
+# handling possible font error
 if (!"Arial" %in% extrafont::fonts()){
   extrafont::font_import(pattern="ari")}
 windowsFonts("Arial" = windowsFont("Arial"))
 
-#function that creates a ggplot and returns a plotly object while printing both
-#rewrites gg_plot object if existing (!!!)
+# function that creates a ggplot and returns a plotly object while printing both
+# rewrites gg_plot object if existing (!!!)
 annual_plot_lines <- function(Data, Lines=country, line_label="Country", gg_name="gg_plot", title){
   #ggplot - annual data  aggregated by Lines
   annual_gg <- Data %>% ggplot(aes(x=year, y=average, 
@@ -70,7 +70,7 @@ ggplot1 <- gg_plot
 #saveWidget(as_widget(annual_plotly), "Annual_sucidie.html")
 
 #-----------------------------------------------------------------------------------
-#male population only
+# male population only
 
 #data
 df2 <- orig[orig$sex=="male",]
@@ -78,27 +78,25 @@ df2 <- orig[orig$sex=="male",]
 df2 <- aggreGate(df2)
 
 
-#creating plots
+# creating plots
 Plotly2 <- annual_plot_lines(df2, title = "Male population")
 ggplot2 <- gg_plot
 #-------------------------------------------------------------------------------
-#females only
+# females only
 
 df3 <- orig[orig$sex=="female",]
 df3 <- aggreGate(df3)
 Plotly3 <- annual_plot_lines(df3, title="Female population")
 ggplot3 <- gg_plot
 #-------------------------------------------------------------------------------
-#aggregate by generation
-
-df4 <- orig
-df4 <- aggreGate(df4, "generation", "year")
+# aggregate by generation
+df4 <- aggreGate(orig, "generation", "year")
 
 Plotly4 <- annual_plot_lines(df4, Lines = generation, line_label = "Generation",
                              title = "Across Generations")
 ggplot4 <- gg_plot
 #-------------------------------------------------------------------------------
-#"top" suicide countries on barplots
+# "top" suicide countries on barplots
 top_suicide_countries <- attributes(head(sort(tapply(df$average, list(df$country), mean),
                                               decreasing = T),6))$dimnames[[1]]
 
@@ -118,7 +116,7 @@ ggplot5 <- ggplot(df5, aes(country, average, group = as.factor(year), fill=as.fa
   theme(legend.title = element_text(face="bold", size=16))+
   scale_fill_hue(l = 50, c = 50)
 
-
+# by coutries and generations
 df6 <- orig[orig$country %in% top_suicide_countries,]
 df6 <- aggreGate(df6, "country", "generation")
 
@@ -131,10 +129,12 @@ ggplot6 <- ggplot(df6, aes(country, average, fill = generation))+
   theme(plot.title = element_text(hjust = 0.5, face = "bold", size=20))+
   theme(axis.title = element_text(face="bold", size=16))+
   theme(legend.title = element_text(size = 16,face="bold"))+
-  scale_fill_brewer(palette="OrRd")
-  
+  scale_fill_brewer(palette="OrRd", direction = -1)
+
+#------------------------------------------------------------------------------
+# Creating subplots
 plot1_2 <- ggplot1 + theme(legend.position = "none")+
-theme(axis.title.x = element_blank())
+  theme(axis.title.x = element_blank())
 
 plot2_2 <- ggplot2 + theme(legend.position = "none")+
   theme(axis.title.x = element_text(size=16))
@@ -155,19 +155,44 @@ year_colors <- as_ggplot(get_legend(plot5))
 main <- grid.arrange(plot1_2,plot5_2,plot3_2,
                      plot6_2,plot2_2,plot4_2,
                      top = textGrob("Suicide rates from 1987 to 2016",
-                                    gp=gpar(fontsize=20,fontfamily="Arial",fontface="bold")))
+                                    gp=gpar(fontsize=20,fontfamily="Arial",
+                                            fontface="bold")))
 
+#-------------------------------------------------------------------------------
+# Saving plots in a subfolder 
+# If Plot folder does not exist in the working directory
+# it will be created there
 
-for (p in ls()[grep("ggplot",ls())]){
-  png(paste(get(p)$labels$title,".png", sep=""), width = 2048, height = 768)
-  print(get(p))
-  dev.off()
+# fun to save plotlys
+save_plotLYs <- function(vector_of_names){
+  dir.create(paste(getwd(), "Plots", sep="/"), showWarnings = F)
+  dir.create(paste(getwd(), "Plots/Plotly", sep="/"), showWarnings = F)
+  for (p in vector_of_names){
+    setwd("./Plots/Plotly")
+    saveWidget(as_widget(get(p)),
+               paste(gsub(".*> | </.*","", get(p)$x$layout$title$text), ".html", sep=""))
+    setwd("../..")
+  }
 }
 
-for (p in ls()[grep("Plotly",ls())]){
-  saveWidget(as_widget(get(p)), 
-             paste(gsub(".*> | </.*","", get(p)$x$layout$title$text), ".html", sep=""))
-} # :)
+# fun to save ggplots in png
+save_GGplots <- function(vector_of_names, w, h){
+  dir.create(paste(getwd(), "Plots", sep="/"), showWarnings = F)
+  dir.create(paste(getwd(), "Plots/GGplot", sep="/"), showWarnings = F)
+  for (p in vector_of_names){
+    setwd("./Plots/GGplot")
+    png(paste(get(p)$labels$title,".png", sep=""), w, h)
+    print({get(p)})
+    dev.off()
+    setwd("../..")
+  }
+}
+
+save_plotLYs(vector_of_names =  ls()[grep("Plotly", ls())])
+
+save_GGplots(vector_of_names =  ls()[grep("ggplot", ls())],
+             w = 2048,
+             h = 768)
 
 # As time passes it seems like the rate of suicide slightly decreases
 # although in some countries it has increased in the past few years. 
